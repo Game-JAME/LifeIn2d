91a4b744be99ceaa081dbcdc7bcd34d5996e91b7
godsonjoseph <jebasinghsundersoni@gmail.com>
2023-05-22 12:11:05 +0530

    Gun created

--- lifeIn2d/Assets/Scripts/Gun.cs
+++ lifeIn2d/Assets/Scripts/Gun.cs
@@ -4,11 +4,55 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-   public float offset;
+  public GameObject bulletPrefab;
+    public float bulletSpeed = 10f;
+
     void Update()
     {
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition)-transform.position;
-        float rotz= Mathf.Atan2(difference.y,difference.x)*Mathf.Rad2Deg;
-         transform.rotation=Quaternion.Euler(0f,0f,rotz+offset);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FireBullet();
+        }
     }
+
+    void FireBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+        bulletRigidbody.velocity = transform.forward * bulletSpeed;
+    }
+    /*
+    public float offset;
+
+    public GameObject projectile;
+    //public GameObject shotEffect;
+    public Transform shotPoint;
+   // public Animator camAnim;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    private void Update()
+    {
+        // Handles the weapon rotation
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+        if (timeBtwShots <= 0)
+        {
+            if (Input.GetMouseButton(0))
+            {
+             //   Instantiate(shotEffect, shotPoint.position, Quaternion.identity);
+            //    camAnim.SetTrigger("shake");
+                Instantiate(projectile, shotPoint.position, transform.rotation);
+                timeBtwShots = startTimeBtwShots;
+            }
+        }
+        else {
+            timeBtwShots -= Time.deltaTime;
+        }
+
+       
+    }*/
 }
