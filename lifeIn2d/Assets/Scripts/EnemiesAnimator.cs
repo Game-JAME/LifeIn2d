4a989f01cd15ad69875eb53c5c6e4bdbb065561c
Salomão Gouveia Santos <55797110+SalomaoGouveiaSantos@users.noreply.github.com>
2023-05-25 11:12:05 -0300

    upd ProjectLife2D
    I try to fix the melee animation using with basis the shootingEnemy code, but doesn't work.

--- lifeIn2d/Assets/Scripts/EnemiesAnimator.cs
+++ lifeIn2d/Assets/Scripts/EnemiesAnimator.cs
@@ -5,25 +5,30 @@ using UnityEngine;
 public class EnemiesAnimator : MonoBehaviour
 {
     [SerializeField] private ShootingEnemy shootingEnemy;
+    [SerializeField] private Enemy enemy;
     private Animator animator;
-     private const string IS_WALKING = "IsWalking";
-      private const string IS_ATTACKING = "IsAttacking";
-      public bool isWalking=false;
-      
+    private const string IS_WALKING = "IsWalking";
+    private const string IS_ATTACKING = "IsAttacking";
+    public bool isWalking = false;
+
     public void Awake()
     {
         animator = GetComponent<Animator>();
-       // animator.SetBool("IsWalking", shootingEnemy.IsWalking());
+        // animator.SetBool("IsWalking", shootingEnemy.IsWalking());
     }
-    void Start(){
-       // player=FindObjectOfType<PlayerMovement>();
-       // shootingEnemy=GetComponent<ShootingEnemy>();
+    void Start()
+    {
+        // player=FindObjectOfType<PlayerMovement>();
+        // shootingEnemy=GetComponent<ShootingEnemy>();
     }
-    void Update(){
-         
-         ///  Debug.Log("Walking");
+    void Update()
+    {
+
+        ///  Debug.Log("Walking");
     }
-    public void SetWalkingAnimation(bool value){
-          animator.SetBool(IS_WALKING, value);
+    public void SetWalkingAnimation(bool value)
+    {
+        animator.SetBool(IS_WALKING, value);
     }
+
 }
