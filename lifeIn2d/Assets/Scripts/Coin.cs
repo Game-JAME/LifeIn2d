057d5451aeecff26427903f861425969690f68a7
Akhilan Y <akhilany123@gmail.com>
2023-05-28 14:10:39 +0530

    coin sound added

--- lifeIn2d/Assets/Scripts/Coin.cs
+++ lifeIn2d/Assets/Scripts/Coin.cs
@@ -6,9 +6,11 @@ public class Coin : MonoBehaviour
 {
     PlayerMovement player;
     // Start is called before the first frame update
+    [SerializeField] AudioSource audio;
     void Start()
     {
         player = FindObjectOfType<PlayerMovement>();
+        audio = GetComponent<AudioSource>();
         // remove coin by default after 2 sec
         Destroy(gameObject,2f);
     }
@@ -19,7 +21,16 @@ public class Coin : MonoBehaviour
         if(col.CompareTag("Player"))
         {
             player.UpdateCoinCount(1);
-            Destroy(gameObject);
+            StartCoroutine(PlayAudio());
         }
+    }
+    IEnumerator PlayAudio()
+    {
+        audio.Play();
+        while (audio.isPlaying)
+        {
+            yield return null;
+        }
+        Destroy(gameObject);
     }
 }
--- lifeIn2d/Assets/Scripts/CoinSpawn.cs
+++ lifeIn2d/Assets/Scripts/CoinSpawn.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class CoinSpawn : MonoBehaviour
 {
-    [SerializeField] Vector3[] coinPositions = {new Vector3(-73.3708725f,-1.53047371f,0.0f),new Vector3(-79.1999969f,27.8999996f,0.0f),new Vector3(-83.0f,-12.6000004f,0.0f)};
+    public Vector3[] coinPositions = {new Vector3(-73.3708725f,-1.53047371f,0.0f),new Vector3(-79.1999969f,27.8999996f,0.0f),new Vector3(-111.699997f,38.4000015f,0.0f),new Vector3(-111.699997f,22.5f,0.0f),new Vector3(-111.699997f,11.5f,0.0f),new Vector3(-111.699997f,-8.5f,0.0f),new Vector3(-111.699997f,-36.9000015f,0.0f),new Vector3(-82.8000031f,-36.9000015f,0.0f),new Vector3(-82.8000031f,-15.8999996f,0.0f),new Vector3(-82.8000031f,10.3999996f,0.0f),new Vector3(-82.8000031f,32f,0.0f)};
     [SerializeField] GameObject coinPrefab;
     
     void Start()
     {
-        InvokeRepeating("SpawnCoin",1f,1f);
+        InvokeRepeating("SpawnCoin",1f,0.5f);
     }
 
     void SpawnCoin()
